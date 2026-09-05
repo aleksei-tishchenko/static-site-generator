270f1ff7b4af3fe5496d8c656d25dbbab94db972
aleksei-tishchenko <alex.032223@gmail.com>
2026-09-05 13:55:44 +0200

    4

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,7 +4,9 @@ from pathlib import Path
 from markdown_blocks import extract_title, markdown_to_html_node
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(
+    from_path: str, template_path: str, dest_path: str | Path, basepath: str
+) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as f:
         markdown_file: str = f.read()
@@ -12,8 +14,11 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
         template_file = f.read()
     html_string = markdown_to_html_node(markdown_file).to_html()
     title = extract_title(markdown_file)
-    content = template_file.replace("{{ Title }}", title).replace(
-        "{{ Content }}", html_string
+    content = (
+        template_file.replace("{{ Title }}", title)
+        .replace("{{ Content }}", html_string)
+        .replace('href="/', f'href="{basepath}')
+        .replace('src="/', f'src="{basepath}')
     )
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -25,28 +30,11 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
 def generate_pages_recursive(
     dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str
 ) -> None:
-    for entity in os.listdir(dir_path_content):
-        full_path = os.path.join(dir_path_content, entity)
-        dest_path = os.path.join(dest_dir_path, "index.html")
-        if os.path.isfile(full_path):
-            with open(full_path, "r") as f:
-                markdown_file: str = f.read()
-            with open(template_path, "r") as f:
-                template_file = f.read()
-            html_string = markdown_to_html_node(markdown_file).to_html()
-            title = extract_title(markdown_file)
-            content = (
-                template_file.replace("{{ Title }}", title)
-                .replace("{{ Content }}", html_string)
-                .replace('href="/', f'href="{basepath}')
-                .replace('src="/', f'src="{basepath}')
-            )
-            full_dest_path = os.path.dirname(dest_path)
-            if full_dest_path != "":
-                os.makedirs(full_dest_path, exist_ok=True)
-            with open(dest_path, "w", encoding="utf-8") as f:
-                f.write(content)
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(
-                full_path, template_path, os.path.join(dest_dir_path, entity), basepath
-            )
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
